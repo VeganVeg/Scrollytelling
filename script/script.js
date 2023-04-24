2ee6a9e9e0dd5118141a89ba8105dcb179110a8d
VeganVeg <90867314+VeganVeg@users.noreply.github.com>
2023-04-24 10:47:42 -0400

    r

--- script/script.js
+++ script/script.js
@@ -1,17 +1,17 @@
 const arrows = document.querySelector('.arrows');
 const scrolling = document.body
 
-const animation = gsap.to('.arrows', { 
-    duration: 0.8,
-    y: '50%',
-    repeat: -1,
-    yoyo: true
-  });
+const animation = gsap.to('.arrows', {
+  duration: 0.8,
+  y: '50%',
+  repeat: -1,
+  yoyo: true
+});
 
-  window.addEventListener('scroll', function(){
-    scrolling.classList.add('is-scrolling');
+window.addEventListener('scroll', function () {
+  scrolling.classList.add('is-scrolling');
 
-    const timer = setTimeout(function() {
-      scrolling.classList.remove('is-scrolling');
-    }, 100)
-  })
+  const timer = setTimeout(function () {
+    scrolling.classList.remove('is-scrolling');
+  }, 100)
+});
