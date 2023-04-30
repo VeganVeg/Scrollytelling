6ca982e33cb86d8633d8cb9c61f4ab7b5e580bf6
VeganVeg <90867314+VeganVeg@users.noreply.github.com>
2023-04-30 12:13:01 -0400

    Update script.js

--- script/script.js
+++ script/script.js
@@ -15,3 +15,19 @@ window.addEventListener('scroll', function () {
     scrolling.classList.remove('is-scrolling');
   }, 100)
 });
+
+let timeline1 = gsap.timeline()
+  .from('.texteC1', {opacity: 0, letterSpacing: '4px', duration: 4})
+  .from('.texteC2', {opacity: 0, letterSpacing: '4px', duration: 4})
+  .from('.texteC3', {opacity: 0, letterSpacing: '4px', duration: 4})
+  .from('.texteC4', {opacity: 0, letterSpacing: '4px', duration: 4})
+  .from('.texteC5', {opacity: 0, letterSpacing: '4px', duration: 4})
+  .from('.texteC6', {opacity: 0, letterSpacing: '4px', duration: 4})
+
+let timeline2 = gsap.timeline()
+  .to('.texteC1', {y:'40px', yoyo: true, repeat: -1, duration: 2})
+  .to('.texteC2', {y:'40px', yoyo: true, repeat: -1, duration: 2}, '<')
+  .to('.texteC3', {y:'40px', yoyo: true, repeat: -1, duration: 2}, '<')
+  .to('.texteC4', {y:'40px', yoyo: true, repeat: -1, duration: 2}, '<')
+  .to('.texteC5', {y:'40px', yoyo: true, repeat: -1, duration: 2}, '<')
+  .to('.texteC6', {y:'40px', yoyo: true, repeat: -1, duration: 2}, '<')
